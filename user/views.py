28a58c6888a0982f87eafe28cc6cc78ae668cf4e
Gyubster <kyunbo0910@naver.com>
2021-02-19 19:59:31 +0900

    CREATE: SingInView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json, bcrypt, jwt
 
-# Create your views here.
+from django.shortcuts   import render
+from django.views       import View
+from django.http        import JsonResponse, HttpResponse
+
+from user.models        import User
+from my_settings        import SECRET_KEY
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        try:
+            user_name   = data['user_name']
+            password    = data['password']
+
+            user = User.objects.get(user_name=user_name)
+            
+            password_validation = bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8'))
+
+            if password_validation:
+                token = jwt.encode({'user_name':user.name}, SECRET_KEY, algorithm='HS256')
+                token = token.decode('utf-8')
+
+                return JsonResponse({'token':token}, status=200)
+        
+            return JsonResponse({'message':'SIGNIN_FAIL'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status=400)
