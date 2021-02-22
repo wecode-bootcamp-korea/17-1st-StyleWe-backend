b5883aa8282434a687ba11b07db9947f5ed1fc9a
Gyubster <kyunbo0910@naver.com>
2021-02-22 19:16:15 +0900

    MODIFY: user/utils.py

--- user/utils.py
+++ user/utils.py
@@ -9,7 +9,7 @@ def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             access_token    = request.headers['Authorization']
-            payload         = jwt.decode(access_token, SECRET_KEY, algorithms=AL)
+            payload         = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
             user            = User.objects.get(id=payload['user_id'])
             request.user    = user
 
