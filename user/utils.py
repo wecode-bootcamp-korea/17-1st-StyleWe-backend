81c8d6ad56dc418c55fe3235893d12326df2a9b0
Gyubster <kyunbo0910@naver.com>
2021-02-22 18:21:09 +0900

    MODIFY: user/utils.py

--- user/utils.py
+++ user/utils.py
@@ -3,7 +3,7 @@ import json
 
 from django.http        import JsonResponse
 from user.models        import User
-from my_settings        import SECRET_KEY, AL
+from my_settings        import SECRET_KEY, ALGORITHM
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
