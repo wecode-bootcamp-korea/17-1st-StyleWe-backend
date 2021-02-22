2c9f6126719ac8e2cf109f36907972c232e88be8
Gyubster <kyunbo0910@naver.com>
2021-02-22 18:55:14 +0900

    MODIFY: user/utils.py

--- user/utils.py
+++ user/utils.py
@@ -3,7 +3,7 @@ import json
 
 from django.http        import JsonResponse
 from user.models        import User
-from my_settings        import SECRET_KEY, AL
+from my_settings        import SECRET_KEY, ALGORITHM
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
@@ -15,7 +15,7 @@ def login_decorator(func):
 
         except jwt.exceptions.DecodeError:
             return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
-        except user.DoesNotExist:
+        except User.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=400)
         return func(self, request, *args, **kwargs)
 
