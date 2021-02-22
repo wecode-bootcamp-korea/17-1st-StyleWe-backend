c181b6adea9751dfd9d9fc51d11570778984e6cc
Gyubster <kyunbo0910@naver.com>
2021-02-22 18:00:59 +0900

    MODIFY: user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls    import path,include
 from user.views     import SignUpFinalizeView
 
 urlpatterns = [
-    path('signup/final', SignUpFinalizeView.as_view()),
+    path('/signup/final', SignUpFinalizeView.as_view()),
 ]
