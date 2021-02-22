b3b2c524aaaf6d41b7ed2bb5dce5009411e14d9e
Gyubster <kyunbo0910@naver.com>
2021-02-22 17:39:38 +0900

    MODIFY: user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls    import path, include
 from user.views     import SignUpInitializeView
 
 urlpatterns = [
-        path('signup/initial', SignUpInitializeView.as_view())        
+        path('/signup/initial', SignUpInitializeView.as_view())        
 ]
