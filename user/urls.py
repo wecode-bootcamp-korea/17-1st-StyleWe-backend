face97adf71fbedf5d1f3686afd1762ea4431a11
Gyubster <kyunbo0910@naver.com>
2021-02-23 14:48:31 +0900

    CREATE: signup/final path created(user/urls.py)

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls    import path, include
-from user.views     import SignUpInitializeView
+from user.views     import UserView
 
 urlpatterns = [
-        path('/signup/initial', SignUpInitializeView.as_view())        
+        path('/signup/initial', UserView.as_view()),
+        path('/signup/fianl', UserView.as_view()),
 ]
