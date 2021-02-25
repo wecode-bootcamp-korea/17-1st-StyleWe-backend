431e9a6be421fcb10befab9e87fe5d5b6b53594c
phin09 <sy6kang@gmail.com>
2021-02-25 21:08:14 +0900

    FIX: missing feed url in config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
         path('user', include('user.urls')),
+        path('feed', include('feed.urls')),
         path('product', include('product.urls'))
 ]
