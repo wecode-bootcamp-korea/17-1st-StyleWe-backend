4e8ab8afb1b395f8036902664943af64f082ca23
phin09 <sy6kang@gmail.com>
2021-02-25 19:36:50 +0900

    MODIFY: handle exceptions in FeedEntireView

--- feed/views.py
+++ feed/views.py
@@ -10,57 +10,61 @@ from user.models        import User
 from feed.models        import Feed, ImageUrl, Comment
 from product.models     import Product, ProductImageUrl
 
-class FeedView(View):    
+class FeedView(View):
     def get(self, request):
-        MAXIMUM_COMMENT = 2
-        feed_list   = []
-        offset      = int(request.GET.get('offset'))
-        limit       = int(request.GET.get('limit'))
+        try:
+            MAXIMUM_COMMENT = 2
+            feed_list   = []
+            offset      = int(request.GET.get('offset', 0))
+            limit       = int(request.GET.get('limit', 30))
+        
+            for feed in Feed.objects.all().order_by('-id'):
+                feed_data = {
+                    'feed_basic_data' : {
+                        'feed_id'         : feed.id, 
+                        'feed_user'       : feed.user.nickname,
+                        'created_at'      : feed.created_at,
+                        'description'     : feed.description,
+                        'like_number'     : feed.like_number,
+                        'tag_item_number' : feed.tag_item_number,
+                        'feed_main_image' : feed.imageurl_set.values('image_url').first()
+                        },
     
-        for feed in Feed.objects.all().order_by('-id'):
-            feed_data = {
-                'feed_basic_data' : {
-                    'feed_id'         : feed.id, 
-                    'feed_user'       : feed.user.nickname,
-                    'created_at'      : feed.created_at,
-                    'description'     : feed.description,
-                    'like_number'     : feed.like_number,
-                    'tag_item_number' : feed.tag_item_number,
-                    'feed_main_image' : feed.imageurl_set.values('image_url').first()
-                    },
-
-                'product_data'   : {
-                    'product_id'     : feed.product_id,
-                    'product_name'   : feed.product.name,
-                    'discount_rate'  : feed.product.discount_rate,
-                    'price'          : feed.product.price,
-                    'product_image'  : feed.product.productimageurl_set.get(is_main=1).image_url
-                    } if feed.product_id else {
-                    'product_id'     : '',
-                    'product_name'   : '',
-                    'discount_rate'  : '',
-                    'price'          : '',
-                    'product_image'  : ''
-                    },
-
-                'feed_comment_data'  : {
-                    'feed_comment_count' : feed.comment_set.count(),
-                    'comment_list'       : [{
-                            'user'      : User.objects.get(id = comment.user_id).nickname,
-                            'content'   : comment.content
-                        } for comment in feed.comment_set.all().order_by('-created_at')[:MAXIMUM_COMMENT]
-                        ] if feed.comment_set.exists() else
-                        [{
-                            'user':'',
-                            'content':''
-                        }]
+                    'product_data'   : {
+                        'product_id'     : feed.product_id,
+                        'product_name'   : feed.product.name,
+                        'discount_rate'  : feed.product.discount_rate,
+                        'price'          : feed.product.price,
+                        'product_image'  : feed.product.productimageurl_set.get(is_main=1).image_url
+                        } if feed.product_id else {
+                        'product_id'     : '',
+                        'product_name'   : '',
+                        'discount_rate'  : '',
+                        'price'          : '',
+                        'product_image'  : ''
+                        },
+    
+                    'feed_comment_data'  : {
+                        'feed_comment_count' : feed.comment_set.count(),
+                        'comment_list'       : [{
+                                'user'      : User.objects.get(id = comment.user_id).nickname,
+                                'content'   : comment.content
+                            } for comment in feed.comment_set.all().order_by('-created_at')[:MAXIMUM_COMMENT]
+                            ] if feed.comment_set.exists() else
+                            [{
+                                'user':'',
+                                'content':''
+                            }]
+                        }
                     }
-                }
+    
+                feed_list.append(feed_data)
+            feed_list = feed_list[offset:offset+limit]
+    
+            return JsonResponse({'feed_list': feed_list}, status=200)
 
-            feed_list.append(feed_data)
-        feed_list = feed_list[offset:offset+limit]
-
-        return JsonResponse({'feed_list': feed_list}, status=200)
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status=404)
 
 class FeedDetailView(View):
     def get(self, request, feed_id):
@@ -120,7 +124,7 @@ class FeedDetailView(View):
             }, status=200)
 
         except ValueError:
-            return JsonResponse({'MESSAGE' : 'INVALID_VALUE TYPE'}, status=400)
+            return JsonResponse({'MESSAGE' : 'INVALID_VALUE_TYPE'}, status=400)
 
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
