f784ca6d44d10e97e02549986e7411329ddd61d3
chaerin <cr434972@gmail.com>
2021-02-25 16:56:41 +0900

    MODIFY: 불필요한 변수 제거in feed views.py

--- feed/views.py
+++ feed/views.py
@@ -15,66 +15,44 @@ class FeedView(View):
         limit       = int(request.GET.get('limit'))
     
         for feed in Feed.objects.all().order_by('-id'):
+            feed_data = {
+                'feed_basic_data' : {
+                    'feed_id'         : feed.id, 
+                    'feed_user'       : feed.user.nickname,
+                    'created_at'      : feed.created_at,
+                    'description'     : feed.description,
+                    'like_number'     : feed.like_number,
+                    'tag_item_number' : feed.tag_item_number,
+                    'feed_main_image' : feed.imageurl_set.values('image_url').first()
+                    },
 
-            # 피드 정보
-            feed_basic_data = {
-                'feed_id'         : feed.id, 
-                'feed_user'       : feed.user.nickname,
-                'created_at'      : feed.created_at,
-                'description'     : feed.description,
-                'like_number'     : feed.like_number,
-                'tag_item_number' : feed.tag_item_number,
-                'feed_main_image' : feed.imageurl_set.values('image_url').first()
-                }
-
-            # 피드의 상품 정보
-            if feed.product_id:
-                product_data = {
+                'product_data'   : {
                     'product_id'     : feed.product_id,
                     'product_name'   : feed.product.name,
                     'discount_rate'  : feed.product.discount_rate,
                     'price'          : feed.product.price,
                     'product_image'  : feed.product.productimageurl_set.get(is_main=1).image_url
-                    }
-            else:
-                product_data = {
+                    } if feed.product_id else {
                     'product_id'     : '',
                     'product_name'   : '',
                     'discount_rate'  : '',
                     'price'          : '',
                     'product_image'  : ''
-                }
-            
-            # 피드의 댓글
-            if feed.comment_set.exists():               
-                feed_comment_new = list(feed.comment_set.all().order_by('-created_at'))[:MAXIMUM_COMMENT]
-                
-                comment_list = [
-                    {
-                        'user'      : User.objects.get(id = comment.user_id).nickname,
-                        'content'   : comment.content
-                    } 
-                    for comment in feed_comment_new
-                ]
+                    },
 
-                feed_comment_data = {
-                    'feed_comment_count'  : feed.comment_set.count(),
-                    'comment_list'        : comment_list                    
+                'feed_comment_data'  : {
+                    'feed_comment_count' : feed.comment_set.count(),
+                    'comment_list'       : [{
+                            'user'      : User.objects.get(id = comment.user_id).nickname,
+                            'content'   : comment.content
+                        } for comment in feed.comment_set.all().order_by('-created_at')[:MAXIMUM_COMMENT]
+                        ] if feed.comment_set.exists() else
+                        [{
+                            'user':'',
+                            'content':''
+                        }]
                     }
-            else:
-                feed_comment_data = {
-                    'feed_comment_count'    : '',
-                    'comment_list'          : [{
-                        'user':'',
-                        'content':''
-                    }]    
                 }
-
-            feed_data = {
-                'feed_basic_data'       : feed_basic_data,
-                'product_data'          : product_data,
-                'feed_comment_data'     : feed_comment_data
-            }
 
             feed_list.append(feed_data)
         feed_list = feed_list[offset:offset+limit]
